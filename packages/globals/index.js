3f57efe84856b7073c83dd8cd3a99313f3171c53
bySabi Files <flxinformatico@gmail.com>
2016-03-07 18:03:10 +0100

    removed Tether import

--- packages/globals/index.js
+++ packages/globals/index.js
@@ -1,5 +1,7 @@
 import jQuery from 'jquery';
-import Tether from 'tether';
 
 global.jQuery = global.$ = global.jquery = jQuery;
-global.Tether = Tether;
+
+// Bootstrap 4 issue: #19005
+global.Tether = {};
+
