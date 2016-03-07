d436ef0ffa05f54557560f0fca132ceb2b97741b
bySabi Files <bysabi.files@gmail.com>
2016-03-07 14:37:19 +0100

    add jquery npm

---
+++ packages/globals/index.js
@@ -0,0 +1,5 @@
+import jQuery from 'jquery';
+import Tether from 'tether';
+
+global.jQuery = global.$ = global.jquery = jQuery;
+global.Tether = Tether;
--- packages/globals/package.js
+++ packages/globals/package.js
@@ -5,5 +5,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.imply('orgztion:meteor-global-tether');
+  api.use('ecmascript');
+  api.mainModule('index.js', 'client');
 });
